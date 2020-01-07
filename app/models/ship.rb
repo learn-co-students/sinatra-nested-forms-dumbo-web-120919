e6ed6f2a6dad0c6fdbfdc147c52f08d0e5a3a99e
Annie Liao <liaowow@gmail.com>
2020-01-07 13:35:08 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_reader :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(name)
+        @name = name
+        @type = type
+        @booty = booty
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        SHIPS.clear
+    end
 end
