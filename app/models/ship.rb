b01e6b3ccfeb31d75754873678a1e5d332659e81
tdonovan79 <tdonovan79@gmail.com>
2020-01-07 17:06:51 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+
+    attr_reader
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        # Ship.all.each do |ship|
+        #     ship.delete
+        # end
+        @@all = []
+    end
+
 end
